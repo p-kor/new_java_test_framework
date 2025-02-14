d2791c6a2c7d2685a3a389b73f09931d9aa4fea3
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-14 20:07:23 +0100

    implement methods to use API endpoints

--- src/test/java/controller/UserController.java
+++ src/test/java/controller/UserController.java
@@ -4,19 +4,19 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import model.User;
 
+import static constant.CommonConstant.BASE_URI;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
 public class UserController {
     RequestSpecification requestSpecification;
-    private static final String BASE_URL = "https://petstore.swagger.io/v2/";
     private static final String USER_ENDPOINT = "user";
 
     public UserController() {
         requestSpecification = given()
                 .accept(JSON)
                 .contentType(JSON)
-                .baseUri(BASE_URL);
+                .baseUri(BASE_URI);
     }
 
     public Response createUser(User user) {
