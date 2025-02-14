48a4ee0f653b0bde22f439cebfe2e0e57ffd1197
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-14 20:41:22 +0100

    add parameterized API test

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,7 +1,13 @@
 import controller.UserController;
 import io.restassured.response.Response;
 import dto.AddUserResponse;
+import model.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -25,7 +31,7 @@ public class ApiTest {
     }
 
     @Test
-    void createUserControllerTest2() {
+    void createInvalidUserControllerTest() {
 
         Response response = userController.createUser(INVALID_USER);
         AddUserResponse createdUserResponse = response.as(AddUserResponse.class); // map to Java object
@@ -33,6 +39,25 @@ public class ApiTest {
         assertEquals(200, response.statusCode());
         assertEquals(200, createdUserResponse.code());
         assertEquals("unknown", createdUserResponse.type());
+        assertEquals("0", createdUserResponse.message());
+    }
+
+    private static List<User> users() {
+
+        return List.of(DEFAULT_USER, INVALID_USER);
+    }
+
+    @ParameterizedTest
+    @MethodSource("users")
+    void createUserParameterizedTest(User user) {
+
+        Response response = userController.createUser(user);
+        AddUserResponse createdUserResponse = response.as(AddUserResponse.class); // map to Java object
+
+        assertEquals(200, response.statusCode());
+        assertEquals(200, createdUserResponse.code());
+        assertEquals("unknown", createdUserResponse.type());
         assertFalse(createdUserResponse.message().isEmpty());
+
     }
 }
